f6b139023dba3a1060fd0a1ec9cb4dd439836eb2
Bill Armstrong <armsty9@gmail.com>
2023-05-16 14:31:52 +0300

    initialized webscocket connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+// use chrono::prelude::*;
+use std::env;
+use web3::types::{ BlockId, BlockNumber };
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let websocket = web3::transports::WebSocket
+        ::new(&env::var("INFURA_MAIN").unwrap())
+        .await
+        .unwrap();
+    let web3s = web3::Web3::new(websocket);
+
+    let latest_block = web3s
+        .eth()
+        .block(BlockId::Number(BlockNumber::Latest))
+        .await
+        .unwrap()
+        .unwrap();
+
+    println!(
+        "block number {}, number of transactions: {},",
+        latest_block.number.unwrap(),
+        &latest_block.transactions.len(),
+       
+    );
 }
