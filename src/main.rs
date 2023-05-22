2ee3b2f7e578892fb632076c5d58f0e473a5a4ae
Bill Armstrong <armsty9@gmail.com>
2023-05-22 12:30:59 +0300

    created a loop to iterate through latest block txns

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // use chrono::prelude::*;
 use std::env;
-use web3::types::{ BlockId, BlockNumber };
+use web3::helpers as w3h;
+use web3::types::H160;
+use web3::types::{ BlockId, BlockNumber, TransactionId, U256, U64 };
 
 #[tokio::main]
 async fn main() {
@@ -20,7 +22,7 @@ async fn main() {
         .unwrap();
 
     println!(
-        "block number {}, parent: {:?}, transactions: {}, gas used: {:?}, gas limit : {:?}",
+        "block number {}, parent: {:?}, transactions: {}, gas used: {:?}, gas limit : {:?},  ",
         latest_block.number.unwrap(),
         latest_block.parent_hash,
         latest_block.transactions.len(),
@@ -29,4 +31,38 @@ async fn main() {
 
        
     );
+
+    for transaction_hash in latest_block.transactions {
+        let tx = match web3s
+            .eth()
+            .transaction(TransactionId::Hash(transaction_hash))
+            .await
+
+        {
+            Ok(Some(tx)) => tx,
+            _=> {
+                println!("Error fetching transaction {:?}", transaction_hash);
+                continue;
+            }
+        };
+
+        let from_addr = tx.from.unwrap_or(H160::zero());
+        let to_addr = tx.to.unwrap_or(H160::zero());
+        let eth_value = wei_to_eth(tx.value);
+        println!(
+            "[{}] from: {:?}, to: {:?}, value: {:?}, gas: {:?}, gas price: {:?}",
+            tx.transaction_index.unwrap_or(U64::from(0)),
+            w3h::to_string(&from_addr),
+            w3h::to_string(&to_addr),
+            eth_value,
+            tx.gas,
+            tx.gas_price 
+        );
+    }
+}
+
+fn wei_to_eth(wei_val: U256) -> f64 {
+    let res = wei_val.as_u128() as f64;
+    let res = res / 1_000_000_000_000_000_000.0;
+    res
 }
